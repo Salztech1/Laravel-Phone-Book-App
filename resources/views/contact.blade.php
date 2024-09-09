5ddade90e2634027cbf4b7ec48a523b5be32452f
salome <salomenjinda0@gmail.com>
2024-09-09 22:16:42 +0100

    added content in contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-<h2>This is my new contact page</h2>
+<h2 class="mt-3 contact">Create Contact</h2>
+<div>
+    <hr />
+    <div class="d-flex">
+        <h2><i class="bi bi-arrow-left-short"></i> </h2>
+        <button style="position: fixed; right: 0; margin: 10px; background-color: #463FF1;color:white; border: none; padding: 5px 5px;">Save Contact <i class="bi bi-box-arrow-in-down"></i></button>
+    </div>
+    <img src="{{URL('images/profile.png')}}" alt="">
+
+</div>
+
 @endsection
