c2e88622d4de59e24fdda561a7aa97a18e79a73c
salome <salomenjinda0@gmail.com>
2024-09-12 11:09:25 +0100

    made updates

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,13 @@
 use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', [PagesController::class, 'showLandingPage'])->name('landing');
-//Route::get('/landing', [PagesController::class, 'showLandingPage'])->name('landing');
+
 Route::get('/', [PagesController::class, 'showLandingPage'])->name('showLandingPage');
 
 
 Route::get('contact-us', [PagesController::class, 'createContact'])->name('contact');
-// Route::get('about-us', [PagesController::class, 'showAboutPage'])->name('about');
-// Route::get('profile-me', [PagesController::class, 'showProfile'])->name('profile');
-// In your web.php
+
 Route::get('/contacts', [PagesController::class, 'index'])->name('contacts.index');
-//Route::get('/contacts/{id}', [PagesController::class, 'showContact'])->name('contacts.show');
-
-
 
 Route::post('contacts', [PagesController::class, 'store'])->name('contacts.store');
 
